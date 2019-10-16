7dea906acd42cb3382dc1f78d73be103adb1a09d
Nuel van Gelder de Neufville <github@neufv.systems>
2019-10-16 16:05:57 +0200

    Add steps

--- main.js
+++ main.js
@@ -1,30 +1,60 @@
-/* global fetch, FormData */
+/* global fetch, FormData, Vue */
 
-const message = document.getElementById('message')
+const app = new Vue({
+  el: '#app',
+  data: {
+    step: 0,
+    mainPicture: null,
+    mainPictureURL: null,
+    productTitle: null,
+    productPrice: null,
+    uploadingPhoto: false,
+    publishing: false
+  },
+  methods: {
+    nextStep: function () {
+      this.step += 1
+    },
+    uploadPhoto: async function () {
+      if (!this.uploadingPhoto) {
+        const pictures = document.getElementById('pictures').files[0]
+        const formData = new FormData()
 
-async function upload () {
-  const title = document.getElementById('title').value
-  const price = document.getElementById('price').value
-  const pictures = document.getElementById('pictures').files[0]
-  const formData = new FormData()
+        this.uploadingPhoto = true
 
-  message.classList.add('active')
-  message.innerHTML = 'Uploading… Hold on'
+        formData.append('pictures', pictures)
 
-  formData.append('pictures', pictures)
+        const response = await fetch('https://assets.e-rob.nl', {method: 'POST', body: formData})
+        this.mainPictureURL = await response.text()
+        this.nextStep()
+      }
+    },
+    resetPhoto: function () {
+      this.uploadingPhoto = false
+      this.mainPicture = null
+      this.mainPictureURL = null
+      this.step = 1
+    },
+    editDetails: function () {
+      this.step = 3
+    },
+    publish: async function () {
+      this.publishing = true
 
-  const response = await fetch('https://assets.e-rob.nl', {method: 'POST', body: formData})
-  const url = await response.text()
+      fetch('https://marketplace.e-rob.nl/api/collections/save/uploads?token=account-311b5b245eceac5ab10804a2c0417d', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          data: {
+            title: this.productTitle,
+            price: this.productPrice,
+            image: this.mainPictureURL }
+        })
+      })
+        .then(res => res.json())
+        .then(entry => console.log(entry))
 
-  fetch('https://marketplace.e-rob.nl/api/collections/save/uploads?token=account-311b5b245eceac5ab10804a2c0417d', {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      data: { title, price, image: url }
-    })
-  })
-    .then(res => res.json())
-    .then(entry => console.log(entry))
-
-  message.innerHTML = 'Your advert was published!'
-}
+      this.step = 5
+    }
+  }
+})
