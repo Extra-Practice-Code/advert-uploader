bee02b04694793ffea36d19b0a787b6cbd593d57
Nuel van Gelder de Neufville <github@neufv.systems>
2019-10-16 14:48:41 +0200

    Add Vue, fix message layout

--- main.js
+++ main.js
@@ -8,24 +8,23 @@ async function upload () {
   const pictures = document.getElementById('pictures').files[0]
   const formData = new FormData()
 
+  message.classList.add('active')
   message.innerHTML = 'Uploading… Hold on'
 
   formData.append('pictures', pictures)
 
-  const response = await fetch('https://assets.e-rob.nl', {method: 'POST', body: formData})
-  const url = await response.text()
+  // const response = await fetch('https://assets.e-rob.nl', {method: 'POST', body: formData})
+  // const url = await response.text()
 
-  console.log(url)
-
-  fetch('https://marketplace.e-rob.nl/api/collections/save/uploads?token=account-311b5b245eceac5ab10804a2c0417d', {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      data: { title, price, image: url }
-    })
-  })
-    .then(res => res.json())
-    .then(entry => console.log(entry))
+  // fetch('https://marketplace.e-rob.nl/api/collections/save/uploads?token=account-311b5b245eceac5ab10804a2c0417d', {
+  //   method: 'post',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({
+  //     data: { title, price, image: url }
+  //   })
+  // })
+  //   .then(res => res.json())
+  //   .then(entry => console.log(entry))
 
   message.innerHTML = 'Your advert was published!'
 }
